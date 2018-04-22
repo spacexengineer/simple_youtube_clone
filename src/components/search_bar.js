f07697f9f4f94902802cebcffbd049ac378ae7ea
spacexengineer <hunter.casbeer@gmail.com>
2018-04-22 12:35:51 -0700

    convert SearchBar to a controlled state component with input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.term} onChange={this.onInputChange} />
+      </div>
+    );
+  }
+
+  onInputChange(e) {
+    this.setState({
+      term: e.target.value
+    });
+  }
+}
 
 export default SearchBar;
