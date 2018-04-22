b430f9b3b3d1122a48f144683921d739fbbce229
spacexengineer <hunter.casbeer@gmail.com>
2018-04-22 11:59:12 -0700

    api added, youtube-api-seach mod added, init search bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+// packages
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <div>Init Setup Test</div>;
+// components
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyDXFPxZk5Je4OSzwSwzLKNjgpD5nW3OPX0";
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector(".container"));
