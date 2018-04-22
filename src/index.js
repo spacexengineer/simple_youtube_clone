b07773d48f65328883a7784be8785858e5c2f10c
spacexengineer <hunter.casbeer@gmail.com>
2018-04-22 14:06:41 -0700

    add more styling to layout; need to move videolist to left of screen

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     // default returns an arry of 5 'video objects'
     YTSearch(
@@ -26,7 +29,8 @@ class App extends Component {
       },
       videos => {
         this.setState({
-          videos
+          videos,
+          selectedVideo: videos[0]
         });
       }
     );
@@ -35,8 +39,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
