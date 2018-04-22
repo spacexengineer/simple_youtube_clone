c07969e7ed0e469a2f376a41d7594ae5915dadb6
spacexengineer <hunter.casbeer@gmail.com>
2018-04-22 12:55:28 -0700

    convert App component to class based component with youtube-search-api initialized

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 // packages
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 // components
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyDXFPxZk5Je4OSzwSwzLKNjgpD5nW3OPX0";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+// configuration for youtube-api-search
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // default returns an arry of 5 'video objects'
+    YTSearch(
+      {
+        key: API_KEY,
+        term: "spacex"
+      },
+      videos => {
+        this.setState({
+          videos
+        });
+      }
+    );
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
